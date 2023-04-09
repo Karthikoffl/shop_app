5e415fbc62b151a88c764d7e0d592cf445aa21b3
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-04-09 22:19:25 +0530

    s8-202

---
+++ lib/providers/cart.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    _items.update(
+      productId,
+      (value) => CartItem(
+        id: value.id,
+        title: value.title,
+        price: value.price,
+        quantity: value.quantity + 1,
+      ),
+      ifAbsent: () => CartItem(
+        id: DateTime.now().toString(),
+        title: title,
+        price: price,
+        quantity: 1,
+      ),
+    );
+    notifyListeners();
+  }
+}
